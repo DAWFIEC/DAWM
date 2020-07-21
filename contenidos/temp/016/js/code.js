9f766e371b75a68e57ec577c8a3de9adcf92a2ff
aavendan <aavendan@gmail.com>
2020-07-21 14:45:04 -0500

    diapositivas por unidad

---
+++ contenidos/temp/016/js/code.js
@@ -0,0 +1,65 @@
+let chatStyle = () => {
+
+    /* Selecciona el nodo con el id "texto-chat" */
+  /* con el método document.getElementById( ) */
+
+  
+  
+  /* Agrega la clase 'textoChat' con el atributo className */
+
+ 
+
+  /* Selecciona el nodo con el id "chat" */
+  /* con el método document.getElementById( ) */
+
+  
+
+  /* 
+    Utiliza los métodos setAttribute y getAttribute para agregar la clase containerChat a las clases que preexistentes en el nodo  
+  */
+
+ 
+
+  
+}
+
+chatStyle();
+
+// Crear la función tooltipStyle 
+// Utiliza el método getElementsByClassName para seleccionar los elementos con la clase 'tooltiptext'
+// para agregar la clase 'tooltipStyle'
+// A cada nodo, agrega la propiedad visibility con el valor hidden
+
+
+
+// Llama a la función tooltipStyle
+
+
+
+// tooltipFunctionality
+
+let tooltipFunctionality = () => {
+  // Selecciona con la clase 'tituloseccion' 
+  let elementos = [] 
+
+  for (let elemento of elementos)  {
+
+    // Agrega un callback manejador al evento onmouseover
+    // Teniendo como referencia el nodo elemento, selecciona el primer elemento con la etiqueta 'div'. 
+    // A este elemento hijo, modifica la propiedad visibility con el valor 'visible'
+
+    
+
+
+    // Agrega un listener al evento mouseout
+    // Teniendo como referencia el nodo elemento, selecciona el primer elemento con la etiqueta 'div'. 
+    // A este elemento hijo, modifica la propiedad visibility con el valor 'hidden'
+
+    
+
+  } 
+}
+
+
+// Llamada a la función tooltipFunctionality
+tooltipFunctionality();
---
+++ contenidos/temp/016/js/loaddata.js
@@ -0,0 +1,10 @@
+let loadUsers = () => {
+
+   
+}
+
+
+//En cuanto el documento esté completamente cargado se ejecutará la función loadUsers
+document.addEventListener('DOMContentLoaded', function() {
+  loadUsers()
+})
